ceadf2c95e2ebfbdbcf5f87bf8c14443e051c828
subin.huang <subin.huang@quncaotech.com>
2016-10-31 22:25:56 +0800

    添加 junit test

---
+++ test-project/src/test/java/com/rubin/test/Test.java
@@ -0,0 +1,15 @@
+package com.rubin.test;
+
+
+/**
+ *
+ * Created by RubinCarter on 2016/10/30.
+ */
+public class Test {
+
+    @org.junit.Test
+    public void test(){
+        System.out.println("test");
+    }
+
+}
